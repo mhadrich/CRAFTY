7f186a4fad7f232f4b8ab9fcd3d36e0cb361feee
mhadrich <hadrichmalek@gmail.com>
2023-10-16 09:33:22 +0100

    link to prodDetails & other

--- crafty/screens/Welcomepage.js
+++ crafty/screens/Welcomepage.js
@@ -3,15 +3,15 @@ import React, { useEffect } from "react";
 const Logoimg = require("../assets/crafty.gif");
 
 const Welcomepage = ({ navigation }) => {
-  useEffect(()=>{
+  useEffect(() => {
     setTimeout(() => {
-      navigation.navigate("ProductDetail")
+      navigation.navigate("SignUp");
     }, 2000);
-  },[])
+  }, []);
 
   return (
     <View className="w-full h-full items-center justify-center bg-white">
-        <Image className="scale-50 pt-60" source={Logoimg} /> 
+      <Image className="scale-50 pt-60" source={Logoimg} />
     </View>
   );
 };
