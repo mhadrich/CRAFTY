e411135e2fe1f51c7ed444c8cc15c5a93b22bdd7
slimani-amine <slimanimedamine24@gmail.com>
2023-10-21 09:15:43 +0100

    some updates

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,20 @@
 const express = require("express");
 const authroute = require("./Routes/authRoute.js");
 const resertRroute = require("./Routes/reset-password-routes.js");
-const ItemRoute = require('./Routes/ItemRoute.js')
-const ArticleRoute = require('./Routes/ArticleRoute.js')
-const OrderRoute = require('./Routes/OrderRoute.js')
-const MaterialRoute = require('./Routes/MaterialRoute.js')
-const CommentRoute = require('./Routes/CommentRoute.js')
-const ImageRoute = require('./Routes/ImageRoute.js')
-const ReviewRoute = require('./Routes/ReviewRoute.js')
-const TagRoute = require('./Routes/TagRoute.js')
-const UserRoute = require('./Routes/UserRoute.js')
-const AdressRoute = require('./Routes/AdressRoute.js')
-const NotificationRoute = require('./Routes/NotificationRoute.js')
-const FavouriteItemRoute = require('./Routes/FavouriteItemRoute.js')
-const WishlistRoute = require('./Routes/WishlistRoute.js')
-const ChatRoute = require('./Routes/chatRoute.js')
-
-
-
-
+const ItemRoute = require("./Routes/ItemRoute.js");
+const ArticleRoute = require("./Routes/ArticleRoute.js");
+const OrderRoute = require("./Routes/OrderRoute.js");
+const MaterialRoute = require("./Routes/MaterialRoute.js");
+const CommentRoute = require("./Routes/CommentRoute.js");
+const ImageRoute = require("./Routes/ImageRoute.js");
+const ReviewRoute = require("./Routes/ReviewRoute.js");
+const TagRoute = require("./Routes/TagRoute.js");
+const UserRoute = require("./Routes/UserRoute.js");
+const AdressRoute = require("./Routes/AdressRoute.js");
+const NotificationRoute = require("./Routes/NotificationRoute.js");
+const FavouriteItemRoute = require("./Routes/FavouriteItemRoute.js");
+const WishlistRoute = require("./Routes/WishlistRoute.js");
+const ChatRoute = require("./Routes/chatRoute.js");
 
 const socket_io = require("socket.io");
 const http = require("http");
@@ -31,15 +27,14 @@ app.use(express.json());
 app.use(cors());
 app.use("/auth", authroute);
 app.use("/reset", resertRroute);
-const server = http.createServer(app)
-const io =  socket_io(server)
-const prisma = require ("./lib/prisma.js")
+const server = http.createServer(app);
+const io = socket_io(server);
+const prisma = require("./lib/prisma.js");
 
-
-io.on('connection', (socket) => {
+io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
 
-  socket.on('joinChat', async ({ userId, otherUserId }) => {
+  socket.on("joinChat", async ({ userId, otherUserId }) => {
     const chat = await prisma.chat.findFirst({
       where: {
         AND: [
@@ -61,45 +56,43 @@ io.on('connection', (socket) => {
     }
   });
 
-  socket.on('sendMessage', async ({ chatId, userId, text }) => {
-    
-    console.log("🚀 ~ file: index.js:93 ~ socket.on ~ text:", text)
-    
+  socket.on("sendMessage", async ({ chatId, userId, text }) => {
+    console.log("🚀 ~ file: index.js:93 ~ socket.on ~ text:", text);
+
     try {
       const message = await prisma.message.create({
         data: {
           text,
-          sender:  userId  ,
-          chatId:   chatId*1 ,
+          sender: userId,
+          chatId: chatId * 1,
         },
       });
 
-      io.to(chatId).emit('message', message);
+      io.to(chatId).emit("message", message);
     } catch (error) {
       console.error(error);
     }
   });
-  
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     console.log(`User disconnected: ${socket.id}`);
   });
 });
 
- app.use("/item",ItemRoute);
-app.use("/article",ArticleRoute);
-app.use("/order",OrderRoute);
-app.use("/material",MaterialRoute);
-app.use("/comment",CommentRoute);
-app.use("/image",ImageRoute);
-app.use("/review",ReviewRoute);
-app.use("/tag",TagRoute);
-app.use("/user",UserRoute);
-app.use("/adress",AdressRoute);
-app.use("/notification",NotificationRoute);
-app.use("/favourite",FavouriteItemRoute);
-app.use("/wishlist",WishlistRoute);
-app.use("/chat",ChatRoute);
+app.use("/item", ItemRoute);
+app.use("/article", ArticleRoute);
+app.use("/order", OrderRoute);
+app.use("/material", MaterialRoute);
+app.use("/comment", CommentRoute);
+app.use("/image", ImageRoute);
+app.use("/review", ReviewRoute);
+app.use("/tag", TagRoute);
+app.use("/user", UserRoute);
+app.use("/adress", AdressRoute);
+app.use("/notification", NotificationRoute);
+app.use("/favourite", FavouriteItemRoute);
+app.use("/wishlist", WishlistRoute);
+app.use("/chat", ChatRoute);
 
 server.listen(PORT, () => {
   console.log(`listening on port :  ${PORT}`);
